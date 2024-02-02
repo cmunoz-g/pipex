054811b595d37a9065e5f5ae7d676bf254f90ad2
Carlos <carlosmg.mkt@gmail.com>
2024-02-02 10:15:33 +0100

    c

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -1,6 +1,6 @@
 #include "pipex.h"
 
-void	child_one(t_pipex *stc, int *fd, char **envp, char **argv)
+void	child_one(t_pipex *stc, int *fd, char **envp, char **argv, int status_one)
 {
 	if (stc->fd_infile < 0)
 		ft_error(argv[1],"No such file or directory", EXIT_FAILURE);
@@ -14,7 +14,7 @@ void	child_one(t_pipex *stc, int *fd, char **envp, char **argv)
 		ft_error("","Could not execute the first cmd", EXIT_FAILURE);
 }
 
-void	child_two(t_pipex *stc, int *fd, char **envp, char **argv)
+void	child_two(t_pipex *stc, int *fd, char **envp, char **argv, int status_two)
 {
 	if (stc->fd_outfile < 0)
 		ft_error(argv[4],"No such file or directory", EXIT_FAILURE);
@@ -23,7 +23,10 @@ void	child_two(t_pipex *stc, int *fd, char **envp, char **argv)
 	close(fd[1]);
 	close(stc->fd_infile);
 	if (!stc->path_cmd_two)
+	{
+		status_two = 127;
 		ft_error(stc->parsed_cmd_two[0],"command not found", 127);
+	}
 	if (execve(stc->path_cmd_two, stc->parsed_cmd_two, envp) == -1)
 		ft_error("","Could not execute the second cmd", EXIT_FAILURE);
 }
@@ -33,12 +36,15 @@ void	status(int status_one, int status_two)
 	int	exit_status;
 
 	exit_status = 0;
+	if (status_one != 0)
+		exit(status_one);
+	else if (status_two != 0)
+		exit(status_two);
 	if (WIFEXITED(status_one) && WEXITSTATUS(status_one) != 0)
 		exit_status = WEXITSTATUS(status_one);
     if (WIFEXITED(status_two) && WEXITSTATUS(status_two) != 0)
 		exit_status = WEXITSTATUS(status_two);
-	if (exit_status != 0)
-		exit(0);
+	exit(exit_status);
 }
 
 void	pipex(t_pipex *stc, char **envp, char**argv)
